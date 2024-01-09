819d7b8f0a79fd2fda51fb0fc40ec36e2b013a11
zzriku <zzriku@users.noreply.github.com>
2024-01-09 01:18:50 +0000

    controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
 class TasksController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def addTodo
+    todo = Todo.new(todo_params)
+    todo.save
+    redirect_to '/todos'
+  end
+
+  def deleteTodo
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to '/todos'
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "" => "todos#index"
+  get "todos" => "todos#index"
+  post "todos" => "todos#addTodo"
+  delete "todos/:id" => "todos#deleteTodo"
 end
